4092ffdf47e9a8c11a1efcf0c715c368ebc46de8
UO276213 <uo276213@uniovi.es>
2022-08-26 17:58:27 +0200

    Falata import httpresponse

--- demo_project/weather_service/views.py
+++ demo_project/weather_service/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.conf import settings
 from os import path
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-def weather_notification_sw(request):
+def weather_notification_sw(_request):
     sw_path = path.join(settings.BASE_DIR, 'weather_service', settings.STATIC_URL[1:-1], 'scripts', 'weatherNotificationSW.js')
     sw = open(sw_path)
     return HttpResponse(sw, content_type='text/javascript')
